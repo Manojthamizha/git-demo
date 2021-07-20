7000c5ae1a79f0cb66b0fcfad1df8571d4d8cdbe
Manojkumar <manojthamizha@gmail.com>
2021-07-20 14:12:02 +0530

    added git-repo project to local repo

---
+++ src/main/java/com/mano/app/git_demo/App.java
@@ -0,0 +1,21 @@
+package com.mano.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public void addData(String Data) {
+		
+		System.out.println("Data = " + Data);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+}
